2ca9008399cb8e6e8126eb43eda1b4682067f9d2
kaviashokkumar123-del <kaviashokkumar123@gmail.com>
2025-10-12 11:08:24 +0530

    Add files via upload
    day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John Doe";
+		int age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+
+		System.out.println(
+				"The Employee Name is :" + name + "\nThe Employee Name age is " + age + "\nThe Employee salary is :"
+						+ salary + "\nThe Employee gender is " + gender + "\nThe Employee acceptTerms is " + acceptTerms
+						+ "\nThe Employee rating is :" + rating + "\nThe Employee mobileNumber is " + mobileNumber
+						+ "\nThe Employee pinCode is :" + pinCode + "\nThe Employee luckyNumber is :" + luckyNumber);
+	}
+
+}
